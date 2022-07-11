add1913d94bad42d4003daf984c66dc2a02f889c
MiKuul <89446067+MiKuul@users.noreply.github.com>
2022-07-11 15:12:07 +0300

    Все ок, но есть одна проблема...

---
+++ js/full-size-photo.js
@@ -0,0 +1,36 @@
+const bigPhoto = document.querySelector('.big-picture');
+const commentsList = document.querySelector('.social__comments');
+
+const createComment = function (array) {
+
+  array.forEach (({avatar, message, name}) => {
+    const comment = document.querySelector('.social__comment');
+    comment.querySelector('.social__picture').src = avatar;
+    comment.querySelector('.social__picture').alt = name;
+    comment.querySelector('.social__text').textContent = message;
+    commentsList.appendChild(comment);
+  });
+};
+
+export const showFullPhoto = function(obj) {
+  const {url, likes, comments, description} = obj;
+  bigPhoto.classList.remove('hidden');
+  bigPhoto.querySelector('.big-picture__img').src = url;
+  bigPhoto.querySelector('.likes-count').textContent = likes;
+  bigPhoto.querySelector('.comments-count').textContent = comments.length;
+  bigPhoto.querySelector('.social__caption').textContent = description;
+
+  createComment (comments);
+  bigPhoto.querySelector('.social__comment-count').classList.add('hidden');
+  bigPhoto.querySelector('.comments-loader').classList.add('hidden');
+  document.querySelector('body').classList.add('modal-open');
+
+  bigPhoto.querySelector('.big-picture__cancel').addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 27) {
+      bigPhoto.classList.add('hidden');
+    }
+  });
+  bigPhoto.querySelector('.big-picture__cancel').addEventListener('click', () => {
+    bigPhoto.classList.add('hidden');
+  });
+};
--- js/thumbnails.js
+++ js/thumbnails.js
@@ -1,19 +1,26 @@
-import {generatePhoto} from './data.js';
+import {showFullPhoto} from './full-size-photo.js';
 
 const pictureList = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-
-
-const createPicture = generatePhoto (25);
-
 const pictureListFragment = document.createDocumentFragment();
 
-createPicture.forEach(({url, likes, comments}) =>  {
+const createPictureElement = (obj) => {
+  const {url, likes, comments} = obj;
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__likes').textContent = likes;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureListFragment.appendChild(pictureElement);
-});
+  pictureElement.addEventListener ('click', () => {
+    showFullPhoto (obj);
+  });
+  return pictureElement;
+};
 
-pictureList.appendChild(pictureListFragment);
+export const renderPhotos = (array) => {
+
+  array.forEach(({url, likes, comments}) => {
+    createPictureElement ({url, likes, comments});
+    pictureList.appendChild(pictureListFragment);
+  });
+};
