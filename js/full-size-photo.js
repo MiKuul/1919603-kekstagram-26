174968d8755c9cd6d51f1500879927ee27a36627
MiKuul <89446067+MiKuul@users.noreply.github.com>
2022-07-19 16:17:29 +0300

    Все поправил

--- js/full-size-photo.js
+++ js/full-size-photo.js
@@ -4,35 +4,13 @@ const modalWindow = document.querySelector('body');
 const moreCommentsButton = document.querySelector('.comments-loader');
 const commentTemplate = document.querySelector('#comment').content.querySelector('.social__comment');
 
-const createComment = function (array) {
-  array.forEach (({avatar, message, name}) => {
-    const commentElement = commentTemplate.cloneNode(true);
-    commentElement.querySelector('.social__picture').src = avatar;
-    commentElement.querySelector('.social__picture').alt = name;
-    commentElement.querySelector('.social__text').textContent = message;
-    commentsList.appendChild(commentElement);
-  });
-};
-
-// Попытка номер 4
-const arraySeparation = function (array) {
-  const size = 5;
-  const subarray = [];
-  for (let i = 0; i < Math.ceil(array.length/size); i++){
-    subarray[i] = array.slice((i*size), (i*size) + size);
-  }
-  moreCommentsButton.addEventListener('click', () => {
-    createComment(subarray[1]);
-  });
-};
-
-const createComments = function (array) {
-  const firstArray = array.slice(0, 5);
-  createComment(firstArray);
-  if (array.length > 5) {
-    moreCommentsButton.classList.remove('hidden');
-    arraySeparation(array);
-  }
+const createComment = function (obj) {
+  const {avatar, message, name} = obj;
+  const commentElement = commentTemplate.cloneNode(true);
+  commentElement.querySelector('.social__picture').src = avatar;
+  commentElement.querySelector('.social__picture').alt = name;
+  commentElement.querySelector('.social__text').textContent = message;
+  return commentElement;
 };
 
 const onKeyDownListener = (evt) => {
@@ -40,6 +18,33 @@ const onKeyDownListener = (evt) => {
     evt.preventDefault();
     closeFullPhoto (); // eslint-disable-line
   }
+};
+
+const renderComments = (arr) => {
+  const fragment = document.createDocumentFragment();
+  arr.forEach((it) => {
+    fragment.appendChild(createComment(it));
+  });
+  commentsList.appendChild(fragment);
+};
+
+const NUM_COMMENTS = 5;
+let start = 0;
+let commentsData = [];
+let commentsCount = 0;
+const renderNextComments = () => {
+  const nextComments = commentsData.slice(start, start + NUM_COMMENTS);
+  renderComments(nextComments);
+  start = start + NUM_COMMENTS;
+
+  if (commentsData.length <= start) {
+    moreCommentsButton.classList.add('hidden');
+  }
+  commentsCount = start;
+  if (start > commentsData.length) {
+    commentsCount = commentsData.length;
+  }
+  bigPhoto.querySelector('.comments-count').textContent = `${commentsCount} из ${  commentsData.length}`;
 };
 
 const closeFullPhoto = function () {
@@ -50,13 +55,14 @@ const closeFullPhoto = function () {
 
 export const showFullPhoto = function(obj) {
   const {url, likes, comments, description} = obj;
+  commentsData = comments;
   bigPhoto.classList.remove('hidden');
-  moreCommentsButton.classList.add('hidden');
+  moreCommentsButton.classList.remove('hidden');
   commentsList.innerHTML = '';
+  start = 0;
+  renderNextComments();
   bigPhoto.querySelector('.big-picture__img img').src = url;
   bigPhoto.querySelector('.likes-count').textContent = likes;
-  createComments (comments);
-  bigPhoto.querySelector('.comments-count').textContent = ` из ${  comments.length}`; // добавить счетчик комментариев
   bigPhoto.querySelector('.social__caption').textContent = description;
 
   modalWindow.classList.add('modal-open');
@@ -65,6 +71,9 @@ export const showFullPhoto = function(obj) {
 };
 
 bigPhoto.querySelector('.big-picture__cancel').addEventListener('click', () => {
-  closeFullPhoto ();
+  closeFullPhoto();
 });
 
+moreCommentsButton.addEventListener('click', () => {
+  renderNextComments();
+});
